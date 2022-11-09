8d86725e9096a780914b024745095dc45b2f169e
Douglas <75459253+dls76@users.noreply.github.com>
2022-11-09 20:43:05 -0300

    add link rotinas
    abre 8 abas de rotina no chrome

---
+++ links.js
@@ -0,0 +1,17 @@
+function abrirLinks() {
+    var sites = [
+    'https://sistemas.usb.org.br/acrm-new/pages/home/home.php', 
+    'https://acrm.sdasystems.org/', 
+    'https://mailadventistas-my.sharepoint.com/:x:/g/personal/douglas_leal_usb_educadventista_org1/EZYI_lKbaelCrakb9SS5sn8BUO-s20HMRqKvfDxKPIRE7Q?e=t3A4E7', 
+    'https://app.powerbi.com/home?noSignUpCheck=1&redirectedFromSignup=1&username=douglas.leal@usb.educadventista.org&AutomaticLogin=true', 
+    'https://caab.escolaemmovimento.com.br/#!/home', 
+    'https://mail.google.com/mail/u/0/#inbox', 
+    'https://mail.google.com/mail/u/1/#inbox', 
+    'https://todoist.com/app/filter/2317565822'] 
+    for (let i = 0; i < sites.length; i++) {
+        window.open(sites[i])
+    }
+};
+
+
+
