b6a85bd449ea741b6ecfd1c28c0f151470b5dcc6
Douglas <75459253+dls76@users.noreply.github.com>
2022-11-11 12:21:04 -0300

    add mais links em rotinas

--- links.js
+++ links.js
@@ -1,13 +1,17 @@
 function abrirLinks() {
     var sites = [
+    'https://mailadventistas-my.sharepoint.com/:x:/g/personal/douglas_leal_usb_educadventista_org1/EZYI_lKbaelCrakb9SS5sn8BUO-s20HMRqKvfDxKPIRE7Q?e=t3A4E7', 
     'https://sistemas.usb.org.br/acrm-new/pages/home/home.php', 
     'https://acrm.sdasystems.org/', 
-    'https://mailadventistas-my.sharepoint.com/:x:/g/personal/douglas_leal_usb_educadventista_org1/EZYI_lKbaelCrakb9SS5sn8BUO-s20HMRqKvfDxKPIRE7Q?e=t3A4E7', 
+    'https://sistema.velabot.com.br/',
     'https://app.powerbi.com/home?noSignUpCheck=1&redirectedFromSignup=1&username=douglas.leal@usb.educadventista.org&AutomaticLogin=true', 
     'https://caab.escolaemmovimento.com.br/#!/home', 
     'https://mail.google.com/mail/u/0/#inbox', 
     'https://mail.google.com/mail/u/1/#inbox', 
-    'https://todoist.com/app/filter/2317565822'] 
+    'https://todoist.com/app/filter/2317565822',
+    'https://calendar.google.com/calendar/u/1/r',
+    'https://www.icloud.com/calendar/'
+    ] 
     for (let i = 0; i < sites.length; i++) {
         window.open(sites[i])
     }
